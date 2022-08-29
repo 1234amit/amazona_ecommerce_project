0fd392f4e2fa487c71dcd5c0e830edc14f6b18dd
Amit Kumar Goswami <cseiubatgoswami@gmail.com>
2022-08-29 22:08:59 +0600

    third commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,48 +2,46 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import data from "./data";
+import { BrowserRouter, NavLink, Route, Routes } from "react-router-dom";
+import HomeScreen from "./Screens/HomeScreen";
+import ProductScreen from "./Screens/ProductScreen";
 
 const App = () => {
   return (
-    <div>
-      <header>
-        <Navbar bg='dark' variant='dark' expand='lg'>
-          <Container>
-            <Navbar.Brand href='#home'>Amazona</Navbar.Brand>
-            <Navbar.Toggle aria-controls='basic-navbar-nav' />
-            <Navbar.Collapse id='basic-navbar-nav'>
-              <Nav className='me-auto'>
-                <Nav.Link href='#home'>Home</Nav.Link>
-                <Nav.Link href='#link'>Link</Nav.Link>
-              </Nav>
-            </Navbar.Collapse>
-          </Container>
-        </Navbar>
-      </header>
+    <BrowserRouter>
+      <div>
+        <header>
+          <Navbar bg='dark' variant='dark' expand='lg'>
+            <Container>
+              <Navbar.Brand as={NavLink} to='/'>
+                Amazona
+              </Navbar.Brand>
+              <Navbar.Toggle aria-controls='basic-navbar-nav' />
+              <Navbar.Collapse id='basic-navbar-nav'>
+                <Nav className='mr-auto right-nav'>
+                  <Nav.Link as={NavLink} to='/cart'>
+                    <i className='fas fa-shopping-cart'></i>
+                    Cart
+                  </Nav.Link>
 
-      <main>
-        <h1 className='text-center'>Featured Product</h1>
-        <div className='products'>
-          {data.products.map((product) => (
-            <div className='product' key={product.slug}>
-              <a href={`/product/${product.slug}`}>
-                <img src={product.image} alt={product.name} />
-              </a>
-              <div className='product-info'>
-                <a href={`/product/${product.slug}`}>
-                  <p>{product.name}</p>
-                </a>
-                <p>
-                  <strong>${product.price}</strong>
-                </p>
-                <button>Add To Cart</button>
-              </div>
-            </div>
-          ))}
-        </div>
-      </main>
-    </div>
+                  <Nav.Link as={NavLink} to='/login'>
+                    <i className='fas fa-sign-in-alt'></i>
+                    Login
+                  </Nav.Link>
+                </Nav>
+              </Navbar.Collapse>
+            </Container>
+          </Navbar>
+        </header>
+
+        <main>
+          <Routes>
+            <Route path='/' element={<HomeScreen />} />
+            <Route path='/product/:slug' element={<ProductScreen />} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 };
 
