8fc658336ed830cdda10108d178300a29c01fa0e
Amit Kumar Goswami <cseiubatgoswami@gmail.com>
2022-09-06 00:49:00 +0600

    create-cart-screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import { BrowserRouter, NavLink, Route, Routes } from "react-router-dom";
+import CartScreen from "./Screens/CartScreen";
 import HomeScreen from "./Screens/HomeScreen";
 import ProductScreen from "./Screens/ProductScreen";
 import { Store } from "./Store";
@@ -49,6 +50,7 @@ const App = () => {
             <Routes>
               <Route path='/' element={<HomeScreen />} />
               <Route path='/product/:slug' element={<ProductScreen />} />
+              <Route path='/cart' element={<CartScreen />} />
             </Routes>
           </Container>
         </main>
